f64eb1e16aca2e991c2fe822e078159f3f1be103
SkiesGgw <108482640+SkiesGgw@users.noreply.github.com>
2023-08-23 21:28:40 +0800

    deposite 法币币种

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -12,7 +12,7 @@ function getApiObj(url, data = {}) {
 }
 
 // const BASE_API = '//test-api.funihash.com';
-const BASE_API = '//api-cdn.beingfi.com';
+const BASE_API = '//api.beingfi.com';
 // const BASE_API = '//192.168.31.173:20004';
 // const BASE_API = '//192.168.31.95:20005';
 const URL_PREFIX = BASE_API + "/fingernft";
