2d6b67c034036eb8f79aa7b077dc055da9cef21e
zwksduan <179310750@qq.com>
2023-08-23 21:29:43 +0800

    正式地址

--- src/app/api/Domain.js
+++ src/app/api/Domain.js
@@ -4,5 +4,6 @@ export default {
 
     /**正式 */
     FUNIBOX_DOMAIN: '//beingfi.com',
-    FUNIBET_API_DOMAIN: '//api.funibet.com',
+    // FUNIBET_API_DOMAIN: '//api.funibet.com',
+    FUNIBET_API_DOMAIN: '//api-source.funibet.com',
 }
