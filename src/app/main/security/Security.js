abc0b75f1dd9536baaa6a27a6b87fbc3cbcf4e56
tianzhaomin <zhaomin.tian001@gmail.com>
2024-09-27 17:10:57 +0800

    [update]解决安全页面首次访问翻译存在的问题

--- src/app/main/security/Security.js
+++ src/app/main/security/Security.js
@@ -1,38 +1,13 @@
 import { useState, useEffect, default as React, useRef } from 'react';
 import Box from '@mui/material/Box';
 import { motion } from 'framer-motion';
-import clsx from 'clsx';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import { styled } from '@mui/material/styles';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
 
 import '../../../styles/home.css';
 import { useSelector, useDispatch } from "react-redux";
 import { selectUserData } from "../../store/user";
-import { tokenTransfer } from "../../store/user/userThunk";
-import BN from "bn.js";
-import StyledAccordionSelect from "../../components/StyledAccordionSelect";
-import { selectConfig } from "../../store/config";
 import { arrayLookup, setPhoneTab } from "../../util/tools/function";
-import { openScan, closeScan } from "../../util/tools/scanqrcode";
-import { getWithDrawConfig, WalletConfigDefineMap, evalTokenTransferFee, getWithdrawHistoryAddress, getWithdrawTransferStats } from "app/store/wallet/walletThunk";
-import DialogContent from "@mui/material/DialogContent/DialogContent";
 import Dialog from "@mui/material/Dialog/Dialog";
-import OtpPass from "../otpPass/OtpPass";
-import DialogTitle from "@mui/material/DialogTitle";
-import CloseIcon from "@mui/material/SvgIcon/SvgIcon";
-import { getCryptoDisplay } from "../../store/wallet/walletThunk";
-import { showMessage } from 'app/store/fuse/messageSlice';
-import MobileDetect from 'mobile-detect';
 import Tabs, { tabsClasses } from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { useTranslation } from "react-i18next";
@@ -45,6 +20,7 @@ import Kyc from "../kyc/Kyc";
 import ResetPin from "../login/ResetPin";
 import { useParams } from 'react-router-dom';
 import history from "@history";
+import {selectCurrentLanguage} from "app/store/i18nSlice";
 
 const container = {
     show: {
@@ -72,12 +48,17 @@ function Security(props) {
 
     const tabValueParam = history.location.state?.tabValue ?? 0
     const resetTabValueParam = history.location.state?.resetTabValueParam ?? 0
+    const currentLanguage = useSelector(selectCurrentLanguage);
     const userData = useSelector(selectUserData);
     const [tabValue, setTabValue] = useState(tabValueParam);
     const [ranges, setRanges] = useState([t('menu_12'), t('menu_13'), t('menu_14'), t('menu_15'), t('menu_16'), "PIN"]);
     useEffect(() => {
         setPhoneTab('security');
     }, []);
+
+    useEffect(() => {
+        setRanges([t('menu_12'), t('menu_13'), t('menu_14'), t('menu_15'), t('menu_16'), "PIN"]);
+    }, [currentLanguage.id]);
 
     return (
         <div>
