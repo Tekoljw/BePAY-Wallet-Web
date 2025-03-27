2d32b6b5609438f27e1b527402a941040e260e21
zwksduan <179310750@qq.com>
2025-03-27 18:09:34 +0800

    还原buy

--- src/app/main/buy/Buy.js
+++ src/app/main/buy/Buy.js
@@ -79,7 +79,6 @@ function Buy(props) {
     };
 
     const [amount, setAmount] = useState('');
-    const [buyAddress, setBuyAddress] = useState('');
     const userData = useSelector(selectUserData);
     const fiatsData = userData.fiat || [];
     const walletData = userData.wallet || {};
@@ -120,13 +119,6 @@ function Buy(props) {
         return res;
     }
 
-    const getRampOption = async () => {
-        let res = await dispatch(getRampPaymentOption());
-        setTimeout(() => {
-            console.log("dddddddddddddddddd", res);
-        }, 2000);
-        return res;
-    }
 
     const getSdkSymbolData = async (payType) => {
         let allSymbols = [];
@@ -246,29 +238,29 @@ function Buy(props) {
         } else if (payType === 'StarPay') {
             getRampOption();
             
-            // if (amount >= fiatObj[currencyCode]?.minAmount && amount <= fiatObj[currencyCode]?.maxAmount) {
-            //     dispatch(getStarPayConfig({
-            //         fiatCurrency: currencyCode,
-            //         amount,
-            //     })).then((res) => {
-            //         let result = res.payload
-            //         if (result.payurl) {
-            //             const loginType = getUserLoginType(userData);
-            //             switch (loginType) {
-            //                 case userLoginType.USER_LOGIN_TYPE_TELEGRAM_WEB_APP: { //telegramWebApp
-            //                     window.location.href = result.payurl
-            //                     break;
-            //                 }
-            //                 default: {
-            //                     window.open(result.payurl)
-            //                     break;
-            //                 }
-            //             }
-            //         }
-            //     });
-            // } else {
-            //     dispatch(showMessage({ message: 'Amount error', code: 2 }));
-            // }
+            if (amount >= fiatObj[currencyCode]?.minAmount && amount <= fiatObj[currencyCode]?.maxAmount) {
+                dispatch(getStarPayConfig({
+                    fiatCurrency: currencyCode,
+                    amount,
+                })).then((res) => {
+                    let result = res.payload
+                    if (result.payurl) {
+                        const loginType = getUserLoginType(userData);
+                        switch (loginType) {
+                            case userLoginType.USER_LOGIN_TYPE_TELEGRAM_WEB_APP: { //telegramWebApp
+                                window.location.href = result.payurl
+                                break;
+                            }
+                            default: {
+                                window.open(result.payurl)
+                                break;
+                            }
+                        }
+                    }
+                });
+            } else {
+                dispatch(showMessage({ message: 'Amount error', code: 2 }));
+            }
 
         }
     };
@@ -451,13 +443,6 @@ function Buy(props) {
                                     </Select>
                                 </FormControl>
 
-
-
-
-
-
-
-
                                 {/*<StyledAccordion*/}
                                 {/*    component={motion.div}*/}
                                 {/*    variants={item}*/}
@@ -517,56 +502,6 @@ function Buy(props) {
                                 {/*</StyledAccordion>*/}
 
                             </Box>
-
-
-                            <Typography className="text-20 font-medium my-16">
-                                {tabValue === 0 && <>
-                                    支付方式
-                                </>}
-                            </Typography>
-
-
-                            {tabValue === 0 && <>
-                                <Box
-                                    className={clsx("w-full rounded-8  flex flex-col my-16 cursor-pointer")}
-                                    sx={{
-                                        backgroundColor: '#1E293B',
-                                        border: "1px solid #1E293B"
-                                    }}
-                                    onClick={() => {
-                                        setPayType('StarPay');
-                                        initSymbolAndFiat();
-                                        getSdkSymbolData('StarPay');
-                                    }}
-                                >
-                                    <StyledAccordion
-                                        component={motion.div}
-                                        variants={item}
-                                        classes={{
-                                            root: 'FaqPage-panel shadow',
-                                        }}
-                                        expanded={expanded === 2}
-                                        onChange={toggleAccordion(2)}
-                                    >
-                                        <div className="flex items-center flex-grow buy-pay-type " style={{ width: '100%', padding: '1.6rem 1.2rem' }}>
-                                            <div className="flex items-center">
-                                                <div style={{
-                                                    width: '30px',
-                                                    borderRadius: '5px',
-                                                }}>
-                                                    <img className='border-r-10' src="wallet/assets/images/buy/wechat_pay.png" alt="" />
-                                                </div>
-                                                <div className="px-12 font-medium">
-                                                    <Typography className="text-20 font-medium">微信</Typography>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </StyledAccordion>
-                                </Box>
-                            </>}
-
-
-
 
                             <Typography className="text-20 font-medium my-16" >{t('home_buy_4')}</Typography>
 
@@ -675,22 +610,6 @@ function Buy(props) {
                                         />
 
                                     </FormControl>
-
-                                    <Typography className="text-20 font-medium my-16" >Address</Typography>
-
-                                    <FormControl sx={{ width: '100%', borderColor: '#94A3B8' }} variant="outlined">
-                                        <OutlinedInput
-                                            id="outlined-adornment-address send-tips-container-address"
-                                            value={buyAddress}
-                                            onChange={(event) => { setBuyAddress(event.target.value) }}
-                                            aria-describedby="outlined-weight-helper-text"
-                                            inputProps={{
-                                                'aria-label': 'address',
-                                            }}
-                                        />
-
-                                    </FormControl>
-
                                 </>}
                             </>}
 
